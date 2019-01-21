f255ff8ea11cf4b507daa24800e88917392fce9d
jacquetta <jacquettavb@hotmail.com>
2019-01-20 23:41:07 -0600

    completed Task 3: array methods assignment

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,35 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(runner){
+    fullName.push(`${runner.first_name} ${runner.last_name}`);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map((runner) => {
+    return allCaps.push({'First_Name': runner.first_name.toUpperCase()});
+    
+});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+
+const largeShirts = runners.filter((runner) => {
+    return runner.shirt_size === 'L';
+})
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((total, runner) => {
+    return total += runner.donation;
+}, 0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -78,6 +92,33 @@ console.log(ticketPriceTotal);
 
 // Problem 1
 
+let company = [];
+runners.forEach(function (runnerCompany) {
+    company.push(`Company:${runnerCompany.company_name} = Participant's Name: ${runnerCompany.first_name} ${runnerCompany.last_name}`);
+});
+console.log(company.sort());
+
 // Problem 2
 
+const donationHigh = runners.filter((runner) => {
+return runner.donation > 150;
+});
+console.log(donationHigh);
+
 // Problem 3
+
+let donationGold = donationHigh.reduce((total, runner) => {
+  return (total += runner.donation);
+}, 0);
+
+console.log(donationGold);
+
+let companyGold = [];
+donationHigh.forEach(function(goldDonation) {
+  companyGold.push(
+    `Participant: ${goldDonation.last_name}, ${goldDonation.first_name} = Donation: ${goldDonation.donation} `
+  );
+});
+console.log(`Gold Companies Total ${donationGold}
+ ${companyGold.sort()}`);
+
